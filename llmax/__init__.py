2f0960ab351f3541a28632a86161b2cfba361546
Maximilien de Dinechin <m.dinechin@protonmail.com>
2024-05-24 12:08:03 +0200

    Export tokens

--- llmax/__init__.py
+++ llmax/__init__.py
@@ -1,10 +1,12 @@
 from . import clients, models, usage, utils
 from .clients import MultiAIClient
+from .usage import tokens
 
 __all__ = [
-    "usage",
     "clients",
-    "utils",
     "models",
     "MultiAIClient",
+    "tokens",
+    "usage",
+    "utils",
 ]
